9ce5173018448f5160b9eef7d967133636964b66
JarateKing <scmacintosh@upei.ca>
2019-08-05 17:48:24 -0300

    added modulo subtraction and division

--- code/modulo_math.cpp
+++ code/modulo_math.cpp
@@ -1,3 +1,5 @@
 #define MOD 1000000007
 #define madd(a,b) (((a%MOD)+(b%MOD))%MOD)
 #define mult(a,b) (((a%MOD)*(b%MOD))%MOD)
+#define msub(a,b) (((a%MOD)-(b%MOD)+MOD)%MOD)
+#define mdiv(a,b) (((a*(1.0/b)+MOD)%MOD)
