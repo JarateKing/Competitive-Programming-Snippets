59bbd5d8b8d6c4338b6e81483691074ef9f96448
JarateKing <scmacintosh@upei.ca>
2019-08-14 23:29:35 -0300

    added compass directions

--- code/directions.cpp
+++ code/directions.cpp
@@ -1,3 +1,7 @@
 // left, down, right, up
 const int dirx[] = {1, 0, -1, 0};
 const int diry[] = {0, 1, 0, -1};
+
+// n ne e se s sw w nw
+const int comx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int comy[] = {1, 1, 0, -1, -1, -1, 0, 1};
