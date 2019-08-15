ad685dc0b4243fbcae6fa1009cf6db450db5605f
JarateKing <scmacintosh@upei.ca>
2019-08-15 00:10:58 -0300

    added 2d array debug print

--- code/debugging.cpp
+++ code/debugging.cpp
@@ -1,3 +1,5 @@
 #define DEBUG(v) cout<<"DEBUG: "<<#v<<" = "<<v<<"\n"
 
 #define DEBUGARR(a,l) cout<<"DEBUG ARRAY: "<<#a<<" = ";for(int ZQ=0;ZQ<l;ZQ++)cout<<a[ZQ]<<" ";cout<<"\n"
+
+#define DEBUGMAT(a,x,y) cout<<"DEBUG 2D: "<<#a<<"\n";for(int XQ=0;XQ<x;XQ++){for(int YQ=0;YQ<y;YQ++)cout<<a[XQ][YQ]<<" ";cout<<"\n";}
