edbdfd6d040159451f3e123f11d0ef056b5fcff5
JarateKing <scmacintosh@upei.ca>
2019-09-23 14:38:13 -0300

    made use modular math funcs

--- code/primality.cpp
+++ code/primality.cpp
@@ -1,25 +1,6 @@
 #define ull unsigned long long
 
-ull mulmod(ull a, ull b, ull m) {
-	ull x = 0;
-	ull y = a % m;
-	while (b > 0) {
-		if (b % 2 == 1) x = (x + y) % m;
-		y = (y * 2) % m;
-		b /= 2;
-	}
-	return x % m;
-}
-ull expmod(ull base, ull e, ull m) {
-	ull x = 1;
-	ull y = base;
-	while (e > 0) {
-		if (e % 2 == 1) x = (x * y) % m;
-		y = (y * y) % m;
-		e /= 2;
-	}
-	return x % m;
-}
+// requires modular math for mult and mpow
 bool isPrime(ull n) {
 	if (n < 2) return false;
 	if (n == 2) return true;
@@ -31,9 +12,9 @@ bool isPrime(ull n) {
 	for (int i = 0; i < 10; i++) {
 		ull temp = s;
 		ull a = rand() % (n - 1) + 1;
-		ull mod = expmod(a, temp, n);
+		ull mod = mpow(a, temp, n);
 		while (temp != n - 1 && mod != 1 && mod != n - 1) {
-			mod = mulmod(mod, mod, n);
+			mod = mult(mod, mod, n);
 			temp *= 2;
 		}
 		if (mod != n - 1 && temp % 2 == 0) return false;
