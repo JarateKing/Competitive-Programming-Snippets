b3b5db0d7d1eccc8997415d8689df6c41604ab58
JarateKing <scmacintosh@upei.ca>
2019-09-23 14:05:06 -0300

    added general purpose divisibility

--- code/modulo_math.cpp
+++ code/modulo_math.cpp
@@ -1,5 +1,23 @@
+#include <algorith> // __gcd
+#define ll long long
+
 #define MOD 1000000007
-#define madd(a,b) (((a%MOD)+(b%MOD))%MOD)
-#define mult(a,b) (((a%MOD)*(b%MOD))%MOD)
-#define msub(a,b) (((a%MOD)-(b%MOD)+MOD)%MOD)
-#define mdiv(a,b) (((a*(1.0/b)+MOD)%MOD)
+#define madd(a,b,m) (((a%m)+(b%m))%m)
+#define mult(a,b,m) (((a%m)*(b%m))%m)
+#define msub(a,b,m) (((a%m)-(b%m)+m)%m)
+
+// if m is not guaranteed to be prime
+ll minv(ll b, ll m) {
+	if (m == 1) return 1;
+	ll swap, oldm = m, q, x = 0, y = 1;
+	while (b > 1) {
+		q = b / m;
+		swap = x, x = y - q * x, y = swap;
+		swap = m, m = b % m, b = swap;
+	}
+	return y + oldm;
+}
+ll mdiv(int a, int b, int m) {
+	if (__gcd(b, m) != 1) return -1;
+	return mult(a, minv(b, m), m);
+}
