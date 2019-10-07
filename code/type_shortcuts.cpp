618ccc9744650a8d2e4c849cec8b2c3bae7992f6
JarateKing <scmacintosh@upei.ca>
2019-10-07 10:03:08 -0300

    added u128 type

--- code/type_shortcuts.cpp
+++ code/type_shortcuts.cpp
@@ -1,4 +1,15 @@
 #define ll long long
 #define ull unsigned long long
+#define u128 unsigned __int128
+
+string u128tos(u128 n) {
+    if (n == 0) return "0";
+    string v = "";
+    while (n > 0) {
+        v = (char)(n % 10 + '0') + v;
+        n /= 10;
+    }
+    return v;
+}
 
 typedef pair<int, int> ii;
