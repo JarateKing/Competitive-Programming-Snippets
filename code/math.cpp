5d1fd63885f5c4417c4d3ffcba7cbf28ee4774ab
JarateKing <scmacintosh@upei.ca>
2019-10-18 14:43:19 -0300

    added egcd

--- code/math.cpp
+++ code/math.cpp
@@ -1,7 +1,20 @@
 #include <algorithm> // __gcd
 
+#define ll long long
 #define ull unsigned long long
 
 ull lcm(ull a, ull b) {
 	return (a * b) / __gcd(a, b);
+}
+
+ll egcd(ll a, ll b, ll& x, ll& y) {
+	if (b == 0) {
+		x = 1;
+		y = 0;
+		return a;
+	}
+	ll gcd = egcd(b, a % b, x, y);
+	x -= a / b * y;
+	ll swap = y; y = x; x = swap;
+	return gcd;
 }
